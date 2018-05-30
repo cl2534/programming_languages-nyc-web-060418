4439f8f5a57a5051be044118062d23a6e7f9b0d8
Changmin Lim <chang.lim92@gmail.com>
2018-05-30 14:44:12 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,23 +1,22 @@
  def reformat_languages(languages)
 
 new_hash = {}
-+
-languages.each do |cat, language|
-+    language.each do |language, types|
-+      types.each do |type, value|
-+        if new_hash[language].nil?
-+          new_hash[language]={}
-+        end
-+        if new_hash[language][:style].nil?
-+          new_hash[language][:style] = []
-+        end
-+        new_hash[language][:style] << cat
-+
-+        if new_hash[language][type].nil?
-+          new_hash[language][type] = value
-+        end
-+      end
-+    end
-+  end
-+  new_hash
- end
+
+languages.each do |symbol, language|
+    language.each do |language, types|
+      types.each do |type, value|
+        if new_hash[language].nil?
+          new_hash[language]={}
+        
+        elsif new_hash[language][:style].nil?
+          new_hash[language][:style] = []
+        elsif new_hash[language][type].nil?
+          new_hash[language][type] = value
+        end
+        new_hash[language][:style] << symbol
+        end
+      end
+    end
+  end
+  new_hash
+end
